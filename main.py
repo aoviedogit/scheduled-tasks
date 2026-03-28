d302aa29ee39fc19f0460fdaf6e42571991200d4
aoviedogit <aoviedo73@yahoo.com>
2026-03-28 00:51:40 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,41 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
-import random
-import smtplib
 import os
+import requests
+from twilio.rest import Client
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+# Twilio account settings to send SMS
+account_sid = os.environ.get("ACCOUNT_SID")
+auth_token = os.environ.get("AUTH_TOKEN")
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+# Openweathermap params to get weather forecast
+Endpoint = "http://api.openweathermap.org/data/2.5/forecast"
+api_key = os.environ.get("OWM_API_KEY")
+MY_LAT = 44.318378
+MY_LONG = 23.796400
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+weather_params = {
+    "lat": MY_LAT,
+    "lon": MY_LONG,
+    "appid": api_key,
+    "cnt": 4
+}
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+# Another way to make a request: Endpoint and params with separate arguments
+# Get weather report every 3 hours for the next 12 hours
+response = requests.get(Endpoint, params=weather_params)
+response.raise_for_status()
+weather_data = response.json()
+
+condition_codes = []
+for items in weather_data["list"]:
+   condition_codes.append(items["weather"][0]["id"])
+
+# If any weather report shows "rain" (condition code < 700) send an SMS or WhatsApp
+if any(x < 700 for x in condition_codes):
+    client = Client(account_sid, auth_token)
+
+    # Send WhatsApp to personal phone
+    message = client.messages.create(
+        from_='whatsapp:+14155238886',
+        body="It's going to rain today, remember to bring an ☂️",
+        to='whatsapp:+16506693642'
+    )
